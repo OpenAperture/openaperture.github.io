77fa05322518e4b4be0559b9aab3f0abadca2764
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-06-19 20:16:45 +0000

    Update deployer api documentation

---
+++ api_docs/deployer/sidebar_items.js
@@ -0,0 +1,23 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.Deployer",
+"docs": ["start/2"]
+},{"id": "OpenAperture.Deployer.Configuration",
+"docs": ["get_current_broker_id/0","get_current_exchange_id/0","get_current_queue_name/0"]
+},{"id": "OpenAperture.Deployer.Dispatcher",
+"docs": ["close_connection/1","init/1","publish/3","publish_rpc/4","spawn_deployment_task/3","start_link/1","subscribe/3","unsubscribe/2"]
+},{"id": "OpenAperture.Deployer.MessageManager",
+"docs": ["remove/1","start_link/0","track/1"]
+},{"id": "OpenAperture.Deployer.Milestones.Deploy",
+"docs": ["deploy/1","start_link/1"]
+},{"id": "OpenAperture.Deployer.Milestones.Monitor",
+"docs": ["log_completed_units/2","log_failed_units/2","log_monitoring_result/4","log_requested_units/2","monitor/2","monitor_remaining_units/5","refresh_systemd_units/2","start_link/1","verify_unit_status/5"]
+},{"id": "OpenAperture.Deployer.Request",
+"docs": ["acknowledge/1","from_payload/2","publish_failure_notification/3","publish_success_notification/2","step_completed/1","step_failed/3"]
+},{"id": "OpenAperture.Deployer.Supervisor",
+"docs": ["init/1","start_link/0"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
