e1dcd8e8078602ea2543c4da48a5c76fd7e0105f
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-06-19 18:18:11 +0000

    Update fleet_manager api documentation

---
+++ api_docs/fleet_manager/sidebar_items.js
@@ -0,0 +1,31 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.FleetManager",
+"docs": ["start/2"]
+},{"id": "OpenAperture.FleetManager.Configuration",
+"docs": ["get_current_broker_id/0","get_current_exchange_id/0","get_current_queue_name/0"]
+},{"id": "OpenAperture.FleetManager.Dispatcher",
+"docs": ["acknowledge/2","close_connection/1","process_request/2","publish/3","publish_rpc/4","register_queues/0","reject/3","start_link/0","subscribe/3","unsubscribe/2"]
+},{"id": "OpenAperture.FleetManager.FleetAction.FleetResponse",
+"docs": ["parse/1"]
+},{"id": "OpenAperture.FleetManager.FleetAction.ListMachines",
+"docs": ["execute/1"]
+},{"id": "OpenAperture.FleetManager.FleetAction.ListUnitStates",
+"docs": ["execute/1"]
+},{"id": "OpenAperture.FleetManager.FleetAction.ListUnits",
+"docs": ["execute/1"]
+},{"id": "OpenAperture.FleetManager.FleetAction.UnitLogs",
+"docs": ["execute/1"]
+},{"id": "OpenAperture.FleetManager.FleetActions",
+"docs": ["execute/1"]
+},{"id": "OpenAperture.FleetManager.MessageManager",
+"docs": ["remove/1","start_link/0","track/1"]
+},{"id": "OpenAperture.FleetManager.Request",
+"docs": ["from_payload/1","to_payload/1"]
+},{"id": "OpenAperture.FleetManager.Supervisor",
+"docs": ["init/1","start_link/0"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
