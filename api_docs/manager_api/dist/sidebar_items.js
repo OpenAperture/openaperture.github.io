03a4eb0030a559367b638be63c0004223fa14d7b
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-08-26 19:58:05 +0000

    Update manager_api api documentation

---
+++ api_docs/manager_api/dist/sidebar_items.js
@@ -0,0 +1,55 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.ManagerApi",
+"docs": ["create/1","create!/1","get_api/0","get_options/1","start/2","start_link/1"]
+},{"id": "OpenAperture.ManagerApi.Deployment",
+"docs": ["create_deployment/6","create_deployment!/6","delete_deployment/6","delete_deployment!/6","execute_deployment/6","execute_deployment!/6","get_deployment/6","get_deployment!/6","list/5","list!/5","update_deployment/7","update_deployment!/7"]
+},{"id": "OpenAperture.ManagerApi.Error",
+"docs": ["process/2"]
+},{"id": "OpenAperture.ManagerApi.EtcdCluster",
+"docs": ["create_cluster/5","create_cluster!/5","delete_cluster/5","delete_cluster!/5","get_cluster/5","get_cluster!/5","get_cluster_machines/5","get_cluster_machines!/5","get_cluster_products/5","get_cluster_products!/5","get_cluster_unit_log/7","get_cluster_unit_log!/7","get_cluster_units/5","get_cluster_units!/5","get_cluster_units_state/5","get_cluster_units_state!/5","list/4","list!/4"]
+},{"id": "OpenAperture.ManagerApi.MessagingBroker",
+"docs": ["broker_connections/5","broker_connections!/5","create_broker/5","create_broker!/5","create_broker_connection/6","create_broker_connection!/6","delete_broker/5","delete_broker!/5","delete_broker_connections/5","delete_broker_connections!/5","get_broker/5","get_broker!/5","list/4","list!/4","update_broker/6","update_broker!/6"]
+},{"id": "OpenAperture.ManagerApi.MessagingExchange",
+"docs": ["create_exchange/5","create_exchange!/5","create_exchange_brokers/6","create_exchange_brokers!/6","delete_exchange/5","delete_exchange!/5","delete_exchange_brokers/5","delete_exchange_brokers!/5","exchange_brokers/5","exchange_brokers!/5","exchange_clusters/5","exchange_clusters!/5","exchange_components/5","exchange_components!/5","exchange_has_modules_of_type?/3","get_exchange/5","get_exchange!/5","list/4","list!/4","update_exchange/6","update_exchange!/6"]
+},{"id": "OpenAperture.ManagerApi.MessagingExchangeModule",
+"docs": ["create_module/6","create_module!/6","delete_module/6","delete_module!/6","get_module/6","get_module!/6","list/5","list!/5"]
+},{"id": "OpenAperture.ManagerApi.MessagingRpcRequest",
+"docs": ["create_request/5","create_request!/5","delete_request/5","delete_request!/5","get_request/5","get_request!/5","list/4","list!/4","update_request/6","update_request!/6"]
+},{"id": "OpenAperture.ManagerApi.Product",
+"docs": ["create_product/2","create_product!/2","delete_product/2","delete_product!/2","get_product/2","get_product!/2","list/1","list!/1","update_product/3","update_product!/3"]
+},{"id": "OpenAperture.ManagerApi.ProductCluster",
+"docs": ["create_clusters/3","create_clusters!/3","delete_clusters/2","delete_clusters!/2","list/2","list!/2"]
+},{"id": "OpenAperture.ManagerApi.ProductComponent",
+"docs": ["create_component/3","create_component!/3","delete_all_components/2","delete_all_components!/2","delete_component/3","delete_component!/3","get_component/3","get_component!/3","list/2","list!/2","update_component/4","update_component!/4"]
+},{"id": "OpenAperture.ManagerApi.ProductDeployment",
+"docs": ["create_deployment/4","create_deployment!/4","delete_deployment/3","delete_deployment!/3","get_deployment/3","get_deployment!/3","get_deployment_steps/3","get_deployment_steps!/3","list/2","list!/2"]
+},{"id": "OpenAperture.ManagerApi.ProductDeploymentPlan",
+"docs": ["create_deployment_plan/3","create_deployment_plan!/3","delete_all_deployment_plans/2","delete_all_deployment_plans!/2","delete_deployment_plan/3","delete_deployment_plan!/3","get_deployment_plan/3","get_deployment_plan!/3","list/2","list!/2","update_deployment_plan/4","update_deployment_plan!/4"]
+},{"id": "OpenAperture.ManagerApi.ProductDeploymentPlanStep",
+"docs": ["create_step/4","create_step!/4","delete_steps/3","delete_steps!/3","list/3","list!/3"]
+},{"id": "OpenAperture.ManagerApi.ProductDeploymentStep",
+"docs": ["create_step/4","create_step!/4","delete_step/4","delete_step!/4","get_step/4","get_step!/4","list/3","list!/3","update_step/5","update_step!/5"]
+},{"id": "OpenAperture.ManagerApi.ProductEnvironment",
+"docs": ["create_environment/3","create_environment!/3","delete_environment/3","delete_environment!/3","get_environment/3","get_environment!/3","list/2","list!/2","update_environment/4","update_environment!/4"]
+},{"id": "OpenAperture.ManagerApi.ProductEnvironmentalVariable",
+"docs": ["create_environment_variable/4","create_environment_variable!/4","create_product_variable/3","create_product_variable!/3","delete_environment_variable/4","delete_environment_variable!/4","delete_product_variable/3","delete_product_variable!/3","get_environment_variable/4","get_environment_variable!/4","get_product_variable/4","get_product_variable!/4","list_environment_variables/3","list_environment_variables!/3","list_product_variables/3","list_product_variables!/3","update_environment_variable/5","update_environment_variable!/5","update_product_variable/4","update_product_variable!/4"]
+},{"id": "OpenAperture.ManagerApi.Resource",
+"docs": ["build_query_string_from_params/1","delete/2","delete/4","get/2","get/4","get_path/1","get_path/2","post/3","post/5","put/3","put/5"]
+},{"id": "OpenAperture.ManagerApi.Response",
+"docs": ["extract_identifier_from_location_header/1","extract_location_header/1","from_httpc_response/1","process/1"]
+},{"id": "OpenAperture.ManagerApi.Supervisor",
+"docs": ["get_api/0","init/1","start_link/0"]
+},{"id": "OpenAperture.ManagerApi.SystemComponent",
+"docs": ["create_system_component/5","create_system_component!/5","delete_system_component/5","delete_system_component!/5","get_system_component/5","get_system_component!/5","list/4","list!/4","update_system_component/6","update_system_component!/6"]
+},{"id": "OpenAperture.ManagerApi.SystemComponentRef",
+"docs": ["create_system_component_ref/5","create_system_component_ref!/5","delete_system_component_ref/5","delete_system_component_ref!/5","get_system_component_ref/5","get_system_component_ref!/5","list/4","list!/4","update_system_component_ref/6","update_system_component_ref!/6"]
+},{"id": "OpenAperture.ManagerApi.SystemEvent",
+"docs": ["create_system_event/5","create_system_event!/5","list/4","list!/4"]
+},{"id": "OpenAperture.ManagerApi.Workflow",
+"docs": ["create_workflow/5","create_workflow!/5","delete_workflow/5","delete_workflow!/5","execute_workflow/6","execute_workflow!/6","get_workflow/5","get_workflow!/5","list/4","list!/4","update_workflow/6","update_workflow!/6"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
