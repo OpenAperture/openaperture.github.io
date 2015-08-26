8fb78b2a2b40e276fc2c6dd37e54f974891095b2
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-08-26 20:05:07 +0000

    Update fleet api documentation

---
+++ api_docs/fleet/dist/sidebar_items.js
@@ -0,0 +1,23 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.Fleet",
+"docs": ["init/1","start/2"]
+},{"id": "OpenAperture.Fleet.CommonSystemdUtils",
+"docs": ["read_output_file/1"]
+},{"id": "OpenAperture.Fleet.EtcdCluster",
+"docs": ["deploy_units/3","get_host_count/1","get_hosts/1"]
+},{"id": "OpenAperture.Fleet.FleetApiInstances",
+"docs": ["get_instance/1","start_link/1"]
+},{"id": "OpenAperture.Fleet.ServiceFileParser",
+"docs": ["parse/1","parse_unit/2"]
+},{"id": "OpenAperture.Fleet.SystemdUnit",
+"docs": ["from_fleet_unit/2","get_fleet_api/1","get_journal/1","get_unit/2","get_units/1","is_active?/1","is_launched?/1","kill_unit/1","spinup_unit/1","teardown_unit/1"]
+},{"id": "OpenAperture.Fleet.SystemdUnit.Journal",
+"docs": ["get_journal/1"]
+},{"id": "OpenAperture.Fleet.SystemdUnit.KillUnit",
+"docs": ["kill_unit/1","kill_unit_on_host/2"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
