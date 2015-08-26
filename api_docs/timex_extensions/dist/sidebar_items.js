4d8520e906799bd7a89c5164ea22f6d02ebfd238
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-08-26 19:53:38 +0000

    Update timex_extensions api documentation

---
+++ api_docs/timex_extensions/dist/sidebar_items.js
@@ -0,0 +1,9 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.Timex.Extensions",
+"docs": ["date_from_unix_timestamp/1","get_elapsed_timestamp/1","seconds_to_time/1","time_from_unix_timestamp/1","unix_timestamp_to_gregorian_seconds/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
