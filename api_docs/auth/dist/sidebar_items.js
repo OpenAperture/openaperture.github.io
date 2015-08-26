e8fbb176d49913b403277bc524b863dcdbe7b532
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-08-26 19:55:55 +0000

    Update auth api documentation

---
+++ api_docs/auth/dist/sidebar_items.js
@@ -0,0 +1,20 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.Auth",
+"docs": ["get_token/0","start/2"]
+},{"id": "OpenAperture.Auth.Client",
+"docs": ["get_token/4","get_token_raw/3"]
+},{"id": "OpenAperture.Auth.Client.Store",
+"docs": ["get/2","put/3","remove/2","start_link/0"]
+},{"id": "OpenAperture.Auth.Server",
+"docs": ["token_info/2","validate_token?/2"]
+},{"id": "OpenAperture.Auth.Server.Store",
+"docs": ["get/2","put/3","remove/2","start_link/0"]
+},{"id": "OpenAperture.Auth.Token"
+},{"id": "OpenAperture.Auth.Util",
+"docs": ["timestamp_add_seconds/2","valid_token?/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
