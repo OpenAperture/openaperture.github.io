0b42ca944c4b7b6e21ff4078d96138a80acb61ff
psw-peppr <trantordevonly@perceptivesoftware.com>
2015-08-26 20:01:05 +0000

    Update overseer_api api documentation

---
+++ api_docs/overseer_api/dist/sidebar_items.js
@@ -0,0 +1,23 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "OpenAperture.OverseerApi",
+"docs": ["start/2"]
+},{"id": "OpenAperture.OverseerApi.Events.Status"
+},{"id": "OpenAperture.OverseerApi.Heartbeat",
+"docs": ["handle_cast/2","publish_status_event/1","set_workload/1","start_link/0"]
+},{"id": "OpenAperture.OverseerApi.ModuleRegistration",
+"docs": ["get_module/0","register_module/1","start_link/0"]
+},{"id": "OpenAperture.OverseerApi.Publisher",
+"docs": ["close_connection/1","handle_cast/2","publish/3","publish_event/1","publish_request/2","publish_rpc/4","start_link/0","subscribe/3","unsubscribe/2"]
+},{"id": "OpenAperture.OverseerApi.Request",
+"docs": ["from_payload/1","to_payload/1"]
+
+  }]
+,  "protocols": [
+    {"id": "OpenAperture.OverseerApi.Events.Event",
+"docs": ["type/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
